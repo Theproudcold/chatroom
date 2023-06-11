ee9cf80b65458a1d50bac80ab36d3bf125979bf0
jiuyan9377 <2297006922@qq.com>
2023-06-11 21:15:25 +0800

    menu菜单基础样式设置

---
+++ src/components/menu.vue
@@ -0,0 +1,55 @@
+<script setup>
+const user = {
+	avatar: "",
+	nickname: "光头强",
+	description: "我是神",
+};
+</script>
+
+<template>
+	<div class="menu">
+		<div class="header">
+			<div class="user-img">
+				<h1>我是头像</h1>
+				<img :src="user.avatar" alt="" />
+			</div>
+			<div class="user-info">
+				<h3 class="nickname">{{ user.nickname }}</h3>
+				<p class="description">{{ user.description }}</p>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.menu {
+	width: 100%;
+	height: 100%;
+	padding: 20px;
+	.header {
+		display: flex;
+		justify-content: center;
+		.user-info {
+			margin-left: 1.125rem;
+			.nickname {
+				width: 7.1875rem;
+				height: 1.9375rem;
+				font-weight: 700;
+				font-size: 1.5rem;
+				line-height: 1.9375rem;
+				letter-spacing: 0.2em;
+				color: $fontColor;
+			}
+			.description {
+				margin-top: 18px;
+				width: 8.1875rem;
+				height: 1.625rem;
+				font-size: 0.625rem;
+				line-height: 0.8125rem;
+				letter-spacing: 0.1em;
+				color: #a8a8a8;
+			}
+		}
+	}
+}
+</style>
--- src/view/index/index.vue
+++ src/view/index/index.vue
@@ -1,11 +1,33 @@
-<script setup></script>
+<script setup>
+import Menu from "@/components/menu.vue";
+import MemberList from "@/components/memberList.vue";
+import ChatWindow from "@/components/chatWindow.vue";
+</script>
 
 <template>
-	<div class="main">
+	<div class="main myCenter">
 		<div class="window">
-			<div class="left"></div>
+			<div class="left">
+				<Menu></Menu>
+			</div>
+			<div class="content">
+				<MemberList></MemberList>
+			</div>
+			<div class="right">
+				<ChatWindow></ChatWindow>
+			</div>
 		</div>
 	</div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.window {
+	padding: 20px;
+	.left {
+		width: 290px;
+		height: 563px;
+		background-color: $mainColor;
+		border-radius: 1.25rem;
+	}
+}
+</style>
