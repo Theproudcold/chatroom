899fc5a4d2094533380a5b94eb4931e67b7fcb78
jiuyan9377 <2297006922@qq.com>
2023-06-15 19:30:29 +0800

    添加时间戳转换函数

--- src/utils/data.js
+++ src/utils/data.js
@@ -46,3 +46,52 @@ export function getFormatDataTimeSecond() {
 	second = second < 10 ? "0" + second : second;
 	return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
 }
+export function getMyDate(str, bool) {
+	if (str > 9999999999) {
+		// 这里判断：时间戳为几位数
+		var c_Date = new Date(parseInt(str));
+	} else {
+		var c_Date = new Date(parseInt(str) * 1000);
+	}
+	var c_Year = c_Date.getFullYear(),
+		c_Month = c_Date.getMonth() + 1,
+		c_Day = c_Date.getDate(),
+		c_Hour = c_Date.getHours(),
+		c_Min = c_Date.getMinutes(),
+		c_Sen = c_Date.getSeconds();
+	if (bool) {
+		// 判断是否需要显示秒
+		var c_Time =
+			c_Year +
+			"-" +
+			getzf(c_Month) +
+			"-" +
+			getzf(c_Day) +
+			" " +
+			getzf(c_Hour) +
+			":" +
+			getzf(c_Min) +
+			":" +
+			getzf(c_Sen); //最后拼接时间
+	} else {
+		var c_Time =
+			c_Year +
+			"-" +
+			getzf(c_Month) +
+			"-" +
+			getzf(c_Day) +
+			" " +
+			getzf(c_Hour) +
+			":" +
+			getzf(c_Min); //最后拼接时间
+	}
+	return c_Time;
+}
+//补0操作  小于10的就在数字前面加0，这应该很好理解吧
+
+function getzf(c_num) {
+	if (parseInt(c_num) < 10) {
+		c_num = "0" + c_num;
+	}
+	return c_num;
+}
