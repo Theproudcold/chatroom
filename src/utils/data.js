2798d6a3544e6f44bccb1a7ba5008fb61c9a8761
jiuyan9377 <2297006922@qq.com>
2023-06-16 00:40:13 +0800

    优化时间戳

--- src/utils/data.js
+++ src/utils/data.js
@@ -18,7 +18,6 @@ export function getFormatData() {
 }
 export function getFormatDataTime() {
 	const currentDate = new Date();
-	const year = currentDate.getFullYear();
 	let month = currentDate.getMonth() + 1;
 	let day = currentDate.getDate();
 	let hour = currentDate.getHours();
@@ -27,7 +26,7 @@ export function getFormatDataTime() {
 	day = day < 10 ? "0" + day : day;
 	hour = hour < 10 ? "0" + hour : hour;
 	minute = minute < 10 ? "0" + minute : minute;
-	return `${year}-${month}-${day} ${hour}:${minute}`;
+	return `${month}-${day} ${hour}:${minute}`;
 }
 export function getFormatDataTimeSecond() {
 	const currentDate = new Date();
@@ -45,46 +44,17 @@ export function getFormatDataTimeSecond() {
 	return `${year}-${month}-${day} ${hour}:${minute}:${second}`;
 }
 // 时间戳转换
-export function getMyDate(str, bool) {
-	if (str > 9999999999) {
-		// 这里判断：时间戳为几位数
-		var c_Date = new Date(parseInt(str));
-	} else {
-		var c_Date = new Date(parseInt(str) * 1000);
-	}
-	var c_Year = c_Date.getFullYear(),
-		c_Month = c_Date.getMonth() + 1,
-		c_Day = c_Date.getDate(),
-		c_Hour = c_Date.getHours(),
-		c_Min = c_Date.getMinutes(),
-		c_Sen = c_Date.getSeconds();
-	if (bool) {
-		// 判断是否需要显示秒
-		var c_Time =
-			c_Year +
-			"-" +
-			getzf(c_Month) +
-			"-" +
-			getzf(c_Day) +
-			" " +
-			getzf(c_Hour) +
-			":" +
-			getzf(c_Min) +
-			":" +
-			getzf(c_Sen); //最后拼接时间
-	} else {
-		var c_Time =
-			c_Year +
-			"-" +
-			getzf(c_Month) +
-			"-" +
-			getzf(c_Day) +
-			" " +
-			getzf(c_Hour) +
-			":" +
-			getzf(c_Min); //最后拼接时间
-	}
-	return c_Time;
+export function getMyDate(str) {
+	const date = new Date(str);
+	const month =
+		date.getMonth() + 1 < 10
+			? "0" + (date.getMonth() + 1)
+			: date.getMonth() + 1;
+	const day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+	const hour = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
+	const minute =
+		date.getMinutes() < 10 ? "0" + date.getMinutes() : date.getMinutes();
+	return `${month}-${day} ${hour}:${minute}`;
 }
 //补0操作  小于10的就在数字前面加0，这应该很好理解吧
 
