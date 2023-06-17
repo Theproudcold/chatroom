820aa62a8cb25bf2a248eb4d083ec29c69693be8
jiuyan9377 <2297006922@qq.com>
2023-06-17 14:24:09 +0800

    更改线上地址

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,4 +1,4 @@
 // const BASE_URL = "localhost";
-const BASE_URL = "192.144.215.50";
+const BASE_URL = "www.chat.api.wrqj.xyz";
 export const WS_ADDRESS = `ws://${BASE_URL}/chat/`;
-export const HTTP_ADDRESS = `http://${BASE_URL}`;
+export const HTTP_ADDRESS = `https://${BASE_URL}`;
