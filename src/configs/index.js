dd962851c0d5798629446e3ff92894b9e15cfda5
jiuyan9377 <2297006922@qq.com>
2023-06-17 14:50:03 +0800

    修复证书问题

--- src/configs/index.js
+++ src/configs/index.js
@@ -1,4 +1,4 @@
 // const BASE_URL = "localhost";
 const BASE_URL = "www.chat.api.wrqj.xyz";
-export const WS_ADDRESS = `ws://${BASE_URL}/chat/`;
+export const WS_ADDRESS = `wss://${BASE_URL}/chat/`;
 export const HTTP_ADDRESS = `https://${BASE_URL}`;
